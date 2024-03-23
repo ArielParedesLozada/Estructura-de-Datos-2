0019cd949b96c1158fced0da5640eca7197bbf98
ArielParedesLozada <152462807+ArielParedesLozada@users.noreply.github.com>
2024-03-23 17:56:14 -0500

    Clase Persona, no hace nada Sólo cambia el método equals() y toString()

---
+++ Entidades/Persona.java
@@ -0,0 +1,31 @@
+package Entidades;
+
+import java.time.LocalDateTime;
+
+public class Persona {
+    public String id;
+    public String nombre, apellido;
+    public String destino;
+    public LocalDateTime horario;
+
+    public Persona(String id, String nombre, String apellido, String destino, LocalDateTime horario){
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.destino = destino;
+        this.horario = horario;
+    }
+
+    @Override
+    public String toString() {
+        return this.id+" "+this.nombre+" "+this.apellido;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        return (obj instanceof Persona other) ? this.id.equals(other.id) : this.id.equals(obj);
+    }
+}
