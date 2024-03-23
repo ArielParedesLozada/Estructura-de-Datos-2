69dac5f920aa138a80289d8ac3d27dbdbad2dfc9
ArielParedesLozada <152462807+ArielParedesLozada@users.noreply.github.com>
2024-03-23 17:53:33 -0500

    Clase del bus, usa un array para almacenar datos No implementa Iterator, aun.

---
+++ Entidades/Bus.java
@@ -0,0 +1,69 @@
+package Entidades;
+
+public class Bus {
+    public String id;
+    private int pasajeros;
+    private Persona[] personas;
+
+    public Bus(String id, int capacidad) {
+        this.id = id;
+        this.personas = new Persona[capacidad];
+        this.pasajeros = 0;
+    }
+
+    public boolean disponible() {
+        return this.pasajeros < this.personas.length;
+    }
+
+    public boolean contains(Persona p) {
+        for (int i = 0; i < this.personas.length; i++) {
+            if (this.personas[i] == null || this.personas[i].equals(p)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean addPasajero(Persona p) {
+        for (int i = 0; i < this.personas.length; i++) {
+            if (this.personas[i] == null) {
+                this.personas[i] = p;
+                this.pasajeros++;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean addPasajeroAt(int index, Persona p) {
+        if (index < this.personas.length && this.personas[index] == null) {
+            this.personas[index] = p;
+            this.pasajeros++;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean outPasajero(Persona p) {
+        for (int i = 0; i < this.personas.length; i++) {
+            if (this.personas[i].equals(p)) {
+                this.personas[i] = null;
+                this.pasajeros--;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public float promedio() {
+        return (this.pasajeros > 0) ? (this.pasajeros / this.personas.length) : 0;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        return (obj instanceof Bus other) ? this.id.equals(other.id) : this.id.equals(obj);
+    }
+}
