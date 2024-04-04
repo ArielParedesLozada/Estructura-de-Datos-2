b5adad2aabb0307e6b61a185a885f38a411de8df
ArielParedesLozada <152462807+ArielParedesLozada@users.noreply.github.com>
2024-04-03 21:38:27 -0500

    Gestor de la Cooperativa Falta bastante

---
+++ Cooperativa/GestorDatos.java
@@ -0,0 +1,13 @@
+package Cooperativa;
+
+import DataStructures.LinkedList;
+import Entidades.Bus;
+import Entidades.Salida;
+
+public class GestorDatos {
+    private LinkedList<Bus> buses;
+    private LinkedList<Salida> salidas;
+    private LinkedList<Historial> historial;
+    
+    
+}
