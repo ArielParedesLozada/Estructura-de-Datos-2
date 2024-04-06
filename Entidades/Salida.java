19ca1e823e10f7594533938d0d7e1ddd07f37fb6
ArielParedesLozada <152462807+ArielParedesLozada@users.noreply.github.com>
2024-04-06 17:12:53 -0500

    Aumenta métodos toString() y asientosBus()

--- Entidades/Salida.java
+++ Entidades/Salida.java
@@ -7,29 +7,38 @@ public class Salida {
     public LocalDateTime horario;
     private Bus bus;
 
-    public Salida(String destino, LocalDateTime horario, Bus bus){
+    public Salida(String destino, LocalDateTime horario, Bus bus) {
         this.destino = destino;
         this.horario = horario;
         this.bus = bus;
     }
 
-    public boolean disponible(Persona p){
+    public boolean disponible(Persona p) {
         return this.horario.equals(p.horario) && this.destino.equals(p.destino) && this.bus.disponible();
     }
-    
-    public boolean addPasajeroAt(int index,Persona p){
+
+    public boolean addPasajeroAt(int index, Persona p) {
         if (disponible(p)) {
             return this.bus.addPasajeroAt(index, p);
         }
         return false;
     }
 
-    public boolean removePasajero(Persona p){
+    public boolean removePasajero(Persona p) {
         return this.bus.outPasajero(p);
     }
-    
-    public boolean betweenTime(LocalDateTime t1, LocalDateTime t2){
+
+    public int[] asientosBus() {
+        return bus.asientosDisponibles();
+    }
+
+    public boolean betweenTime(LocalDateTime t1, LocalDateTime t2) {
         return this.horario.isAfter(t1) && this.horario.isBefore(t2);
+    }
+
+    @Override
+    public String toString() {
+        return this.destino + " " + this.horario.toString();
     }
 
 }
