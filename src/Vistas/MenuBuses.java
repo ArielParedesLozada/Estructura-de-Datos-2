c9b2216b078d5f69a9cbeeef8be94d68b4780215
ArielParedesLozada <152462807+ArielParedesLozada@users.noreply.github.com>
2024-04-25 20:13:43 -0500

    Clase Menu para crear un bus y añadirlo al gestor

--- src/Vistas/MenuBuses.java
+++ src/Vistas/MenuBuses.java
@@ -71,8 +71,8 @@ public class MenuBuses extends javax.swing.JFrame {
 
         jTxtID.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
 
-        jBtnRegresar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/REGRESAR.png"))); // NOI18N
         jBtnRegresar.setBackground(new java.awt.Color(255, 255, 204));
+        jBtnRegresar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Vistas/Imagenes/REGRESAR.png"))); // NOI18N
         jBtnRegresar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jBtnRegresarActionPerformed(evt);
@@ -190,38 +190,6 @@ public class MenuBuses extends javax.swing.JFrame {
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(MenuBuses.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(MenuBuses.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(MenuBuses.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(MenuBuses.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new MenuBuses().setVisible(true);
-            }
-        });
-    }
-
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private java.awt.Button jBtnGuardar;
     private javax.swing.JButton jBtnRegresar;
