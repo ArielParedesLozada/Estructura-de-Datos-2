c1a21a8d03bcd0a9c460fc7f11b4f63a594eb703
ArielParedesLozada <152462807+ArielParedesLozada@users.noreply.github.com>
2024-04-26 12:44:23 -0500

    Push del proyecto inicial CON ERRORES

--- src/Entidades/Salida.java
+++ src/Entidades/Salida.java
@@ -3,75 +3,44 @@ package Entidades;
 import java.time.LocalDateTime;
 
 public class Salida {
-
     public String destino;
     public LocalDateTime horario;
     private Bus bus;
-    private Persona[] pasajeros;
-    private int cantidad;
 
     public Salida(String destino, LocalDateTime horario, Bus bus) {
         this.destino = destino;
         this.horario = horario;
         this.bus = bus;
-        this.pasajeros = new Persona[this.bus.capacidad];
-        this.cantidad = 0;
     }
 
     public boolean disponible(Persona p) {
-        return this.horario.equals(p.horario) && this.destino.equals(p.destino) && (this.bus.capacidad > this.cantidad);
-    }
-    
-    public boolean contains(Persona p) {
-        for (int i = 0; i < this.bus.capacidad; i++) {
-            if (this.pasajeros[i] != null && this.pasajeros[i].equals(p)) {
-                return true;
-            }
-        }
-        return false;
+        return this.horario.equals(p.horario) && this.destino.equals(p.destino) && this.bus.disponible();
     }
 
     public boolean addPasajeroAt(int index, Persona p) {
-        if (disponible(p) && index < this.bus.capacidad && index >= 0 && this.pasajeros[index] == null) {
-            this.pasajeros[index] = p;
-            this.cantidad++;
-            return true;
+        if (disponible(p)) {
+            return this.bus.addPasajeroAt(index, p);
         }
         return false;
     }
 
     public boolean removePasajero(Persona p) {
-        for (int i = 0; i < this.bus.capacidad; i++) {
-            if (this.pasajeros[i].equals(p)) {
-                this.pasajeros[i] = null;
-                this.cantidad--;
-                return true;
-            }
-        }
-        return false;
+        return this.bus.outPasajero(p);
     }
 
-    public int[] asientosDisponibles() {
-        int[] asientos = new int[this.bus.capacidad - this.cantidad];
-        int index = 0;
-        for (int i = 0; i < this.bus.capacidad; i++) {
-            if (this.pasajeros[i] == null) {
-                asientos[index] = i;
-                index++;
-            }
-        }
-        return asientos;
+    public int[] asientosBus() {
+        return bus.asientosDisponibles();
     }
 
     public boolean betweenTime(LocalDateTime t1, LocalDateTime t2) {
         return this.horario.isAfter(t1) && this.horario.isBefore(t2);
     }
-
-    public float getPromedio() {
-        return 100*this.cantidad/this.pasajeros.length;
+    
+    public float getPromedioBus(){
+        return this.bus.promedio();
     }
-
-    public String getIDBus() {
+    
+    public String getIDBus(){
         return this.bus.id;
     }
 
@@ -80,14 +49,4 @@ public class Salida {
         return this.destino + " " + this.horario.toString();
     }
 
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj instanceof Salida) {
-            return ((Salida) obj).bus == this.bus && ((Salida) obj).horario == this.horario && ((Salida) obj).destino.equals(this.destino);
-        }
-        return false;
-    }
 }
