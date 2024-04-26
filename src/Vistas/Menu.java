6fb53e84af2f97a6663e2c1ef73928094305d042
ArielParedesLozada <152462807+ArielParedesLozada@users.noreply.github.com>
2024-04-25 20:07:57 -0500

    Clase Menu para hacer buses, salidas y personas

--- src/Vistas/Menu.java
+++ src/Vistas/Menu.java
@@ -1,5 +1,7 @@
 package Vistas;
 
+import Vistas.Informes.MenuInformes;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
@@ -33,6 +35,7 @@ public class Menu extends javax.swing.JFrame {
         jBtnBoletos = new javax.swing.JButton();
         jBtnSalidas = new javax.swing.JButton();
         jBtnSalir = new javax.swing.JButton();
+        jBtnInformes = new javax.swing.JToggleButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setUndecorated(true);
@@ -44,7 +47,7 @@ public class Menu extends javax.swing.JFrame {
         jLblMenu.setText("MENU PRINCIPAL");
 
         jBtnBuses.setBackground(new java.awt.Color(204, 204, 255));
-        jBtnBuses.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/iconovh.png"))); // NOI18N
+        jBtnBuses.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Vistas/Imagenes/iconovh.png"))); // NOI18N
         jBtnBuses.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jBtnBusesActionPerformed(evt);
@@ -52,7 +55,7 @@ public class Menu extends javax.swing.JFrame {
         });
 
         jBtnBoletos.setBackground(new java.awt.Color(204, 204, 255));
-        jBtnBoletos.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/iconovs.png"))); // NOI18N
+        jBtnBoletos.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Vistas/Imagenes/iconovs.png"))); // NOI18N
         jBtnBoletos.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jBtnBoletosActionPerformed(evt);
@@ -60,7 +63,7 @@ public class Menu extends javax.swing.JFrame {
         });
 
         jBtnSalidas.setBackground(new java.awt.Color(204, 204, 255));
-        jBtnSalidas.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/iconovj.png"))); // NOI18N
+        jBtnSalidas.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Vistas/Imagenes/iconovj.png"))); // NOI18N
         jBtnSalidas.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jBtnSalidasActionPerformed(evt);
@@ -68,10 +71,17 @@ public class Menu extends javax.swing.JFrame {
         });
 
         jBtnSalir.setBackground(new java.awt.Color(255, 255, 204));
-        jBtnSalir.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/EXIT.png"))); // NOI18N
+        jBtnSalir.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Vistas/Imagenes/EXIT.png"))); // NOI18N
         jBtnSalir.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jBtnSalirActionPerformed(evt);
+            }
+        });
+
+        jBtnInformes.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Vistas/Imagenes/informe.png"))); // NOI18N
+        jBtnInformes.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jBtnInformesActionPerformed(evt);
             }
         });
 
@@ -89,7 +99,9 @@ public class Menu extends javax.swing.JFrame {
                 .addGap(54, 54, 54)
                 .addComponent(jBtnBuses, javax.swing.GroupLayout.PREFERRED_SIZE, 138, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(55, 55, 55)
-                .addComponent(jBtnBoletos, javax.swing.GroupLayout.PREFERRED_SIZE, 138, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGroup(jPnl0Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jBtnInformes, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jBtnBoletos, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 57, Short.MAX_VALUE)
                 .addComponent(jBtnSalidas, javax.swing.GroupLayout.PREFERRED_SIZE, 138, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(53, 53, 53))
@@ -109,20 +121,22 @@ public class Menu extends javax.swing.JFrame {
                     .addComponent(jBtnSalidas, javax.swing.GroupLayout.PREFERRED_SIZE, 111, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jBtnBoletos, javax.swing.GroupLayout.PREFERRED_SIZE, 111, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jBtnBuses, javax.swing.GroupLayout.PREFERRED_SIZE, 111, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(44, Short.MAX_VALUE))
+                .addGap(29, 29, 29)
+                .addComponent(jBtnInformes, javax.swing.GroupLayout.PREFERRED_SIZE, 97, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(41, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addComponent(jPnl0, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 0, Short.MAX_VALUE))
+            .addComponent(jPnl0, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPnl0, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(jPnl0, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
         );
 
         pack();
@@ -150,44 +164,20 @@ public class Menu extends javax.swing.JFrame {
         System.exit(0);
     }//GEN-LAST:event_jBtnSalirActionPerformed
 
+    private void jBtnInformesActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBtnInformesActionPerformed
+        MenuInformes mi = new MenuInformes();
+        mi.setVisible(true);
+        this.dispose();
+    }//GEN-LAST:event_jBtnInformesActionPerformed
+
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Menu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Menu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Menu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Menu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new Menu().setVisible(true);
-            }
-        });
-    }
-
+    
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jBtnBoletos;
     private javax.swing.JButton jBtnBuses;
+    private javax.swing.JToggleButton jBtnInformes;
     private javax.swing.JButton jBtnSalidas;
     private javax.swing.JButton jBtnSalir;
     private javax.swing.JLabel jLblMenu;
